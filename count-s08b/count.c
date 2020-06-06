244e2d304bea1f6fb8a74381dff2a8aa5184d080
Philipp Klaus Krause <pkk@spth.de>
2020-06-06 14:38:30 +0200

    Examples for PFC161 and PFS172.

--- count-s08b/count.c
+++ count-s08b/count.c
@@ -1,5 +1,5 @@
-// A 2-bit counter for the Padauk PFC151, PFC154, PFC161, PFS154 and PFS173, to be compiled with SDCC.
-// Counts seconds from 0 to 3 in binary using the 4 LED on the lowest and highest bit of port A.
+// A 2-bit counter for the Padauk PFC161, to be compiled with SDCC.
+// Counts seconds from 0 to 3 in binary using the 4 LED on the lowest bit of port b and highest bit of port A.
 // Written by Philipp Klaus Krause 2019-2020.
 // Source code under CC0 1.0.
 
@@ -12,15 +12,8 @@ __sfr __at(0x05) intrq;
 __sfr __at(0x0b) ihrcr;
 __sfr __at(0x10) pa;
 __sfr __at(0x11) pac;
-#ifdef __SDCC_pdk15 // PFS173
-__sfr __at(0x30) tm2c;
-__sfr __at(0x32) tm2s;
-__sfr __at(0x33) tm2b;
-#else // PFC151, PFC154, PFC161 or PFS154
-__sfr __at(0x1c) tm2c;
-__sfr __at(0x17) tm2s;
-__sfr __at(0x09) tm2b;
-#endif
+__sfr __at(0x14) pb;
+__sfr __at(0x15) pbc;
 
 typedef unsigned long int clock_t;
 #define CLOCKS_PER_SEC 1000ul
@@ -53,11 +46,7 @@ clock_t clock(void)
 
 unsigned char _sdcc_external_startup(void)
 {
-#ifdef __SDCC_pdk15
-	ihrcr = *((const unsigned char*)(0x8bed)); // Use PFS173 factory calibration value for IHRC at 16 Mhz.
-#else
 	ihrcr = *((const unsigned char*)(0x87ed)); // Use PFS154 factory calibration value for IHRC at 16 Mhz.
-#endif
 
 	clkmd = 0x34; // Use IHRC / 2 = 8 Mhz for system clock, disable watchdog.
 	clkmd = 0x30; // Disable ILRC
@@ -77,12 +66,14 @@ __asm
 	engint
 __endasm;
 
-	pac = 0x81;
+	pbc = 0x01;
+	pac = 0x80;
 
 	for(;;)
 	{
 		uint_fast8_t s = (clock() / 1000) % 4;
-		pa = (s << 6 & 0x80) | (s & 0x01);
+		pb = s & 0x01;
+		pa = s << 6 & 0x80;
 	}
 }
 
